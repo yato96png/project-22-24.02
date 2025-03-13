33e07dfd64d2b33c7815097c046fd623807896dc
vizal123 <yato9png@gmail.com>
2025-03-13 13:08:57 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-// frontend/src/App.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  // Делаем запрос к API при загрузке компонента
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/data')  // Запрос к backend
-      .then(response => response.json())
-      .then(data => setData(data))  // Обновляем состояние данными из ответа
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);  // Пустой массив, чтобы запрос выполнился только при монтировании компонента
+    fetch("http://127.0.0.1:5000/api/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+      .catch((error) => console.error("Ошибка загрузки:", error));
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Данные с сервера:</h1>
+    <div>
+      <h1>Список пользователей</h1>
       <ul>
-        {data.map(item => (
-          <li key={item.id}>
-            <strong>{item.name}</strong>: {item.description}
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.FIO}) - {user.phone}
           </li>
         ))}
       </ul>
